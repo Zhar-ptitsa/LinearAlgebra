2d0e6370dd9151d1516ea579afad0083789b3788
stuyphonner <phonner@schools.nyc.gov>
2025-03-19 22:13:45 -0400

    Add files via upload

---
+++ 2D Data Starter.py
@@ -0,0 +1,39 @@
+# Import the matplotlib package and alias the pyplot module 
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Create the plot object
+fig, ax = plt.subplots()
+
+# Set the viewing window in the plot from -50 to 50  in both directions
+ax.set_xlim(-50,50)
+ax.set_ylim(-50,50)
+
+# Read in the data
+# Data format: x,y,
+
+xdata = []
+ydata = []
+
+f = open("2D_data.txt")
+data = f.readlines()
+f.close()
+
+# Parse the data into lists of x's and y's
+
+for i in range(len(data)):
+  datapoint = data[i].split(',')
+  xdata.append(float(datapoint[0]))
+  ydata.append(float(datapoint[1]))
+  
+
+# Here we define the set of x- and y-coordinates beforehand
+# The 'ro' option yields red unconnected dots
+
+plt.plot(xdata,ydata, 'r+')
+
+# This plots the line from (0,5) to (20,1), i.e. the line y = -0.2x + 5 
+plt.plot([-25,25], [-50,50],'b')
+
+# This shows the object (Must ctrl-c out of this in the console)
+plt.show() 
---
+++ 3D Data Starter.py
@@ -0,0 +1,34 @@
+# Import the matplotlib package and alias the pyplot module 
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Library for 3D plots
+from mpl_toolkits.mplot3d import Axes3D
+
+# Create the plot object
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
+xdata  =[]
+ydata = []
+zdata = []
+
+# Read in the data
+# Data format: x,y,z
+
+f = open("3D_data.txt")
+data = f.readlines()
+f.close()
+
+# Parse the data into lists of x's, y's, and z's
+for i in range(len(data)):
+  datapoint = data[i].split(',')
+  xdata.append(float(datapoint[0]))
+  ydata.append(float(datapoint[1]))
+  zdata.append(float(datapoint[2]))
+
+# Create the 3D plot
+ax.scatter3D(xdata,ydata,zdata, c=zdata, cmap = 'Greens')
+
+plt.show()
+
